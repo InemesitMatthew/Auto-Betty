c7f2adca59431ea49c8606673baf77f3e75820ae
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-22 09:23:23 +0000

    Installation Script Update

--- autoBetty/installAutoBetty.sh
+++ autoBetty/installAutoBetty.sh
@@ -5,6 +5,9 @@ SCRIPT_NAME="auto_betty.sh"
 TARGET_DIR="$HOME/bin"
 TARGET_SCRIPT="auto_betty"
 
+# Create the target directory if it doesn't exist
+mkdir -p "$TARGET_DIR"
+
 # Find the location of the auto_betty.sh script in the current directory or subdirectories
 SCRIPT_PATH=$(find . -type f -name "$SCRIPT_NAME" | head -n 1)
 
@@ -17,8 +20,20 @@ fi
 # Copy the found script to the target directory with the desired name
 cp "$SCRIPT_PATH" "$TARGET_DIR/$TARGET_SCRIPT"
 
+# Check if the copy was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to copy $SCRIPT_NAME to $TARGET_DIR."
+    exit 1
+fi
+
 # Make the script executable
 chmod +x "$TARGET_DIR/$TARGET_SCRIPT"
+
+# Check if the permission change was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to make $TARGET_SCRIPT executable."
+    exit 1
+fi
 
 # Display a success message
 echo "Auto Betty script has been installed and can be used globally."
