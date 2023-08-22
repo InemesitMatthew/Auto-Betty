20640f62f0e5ae6ee96905e570dd32e48901667d
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-22 09:33:15 +0000

    Install Script Ver 2: This script will install the auto_betty script in both the user's ~/bin directory and the system-wide /usr/local/bin directory, making it accessible for both local and global use.

--- autoBetty/installAutoBetty.sh
+++ autoBetty/installAutoBetty.sh
@@ -2,11 +2,12 @@
 
 # Define the name of the script to install
 SCRIPT_NAME="auto_betty.sh"
-TARGET_DIR="$HOME/bin"
+TARGET_USER_DIR="$HOME/bin"
+TARGET_SYSTEM_DIR="/usr/local/bin"
 TARGET_SCRIPT="auto_betty"
 
-# Create the target directory if it doesn't exist
-mkdir -p "$TARGET_DIR"
+# Create the target user directory if it doesn't exist
+mkdir -p "$TARGET_USER_DIR"
 
 # Find the location of the auto_betty.sh script in the current directory or subdirectories
 SCRIPT_PATH=$(find . -type f -name "$SCRIPT_NAME" | head -n 1)
@@ -17,23 +18,41 @@ if [ -z "$SCRIPT_PATH" ]; then
     exit 1
 fi
 
-# Copy the found script to the target directory with the desired name
-cp "$SCRIPT_PATH" "$TARGET_DIR/$TARGET_SCRIPT"
+# Copy the found script to the user's directory with the desired name
+cp "$SCRIPT_PATH" "$TARGET_USER_DIR/$TARGET_SCRIPT"
 
 # Check if the copy was successful
 if [ $? -ne 0 ]; then
-    echo "Error: Failed to copy $SCRIPT_NAME to $TARGET_DIR."
+    echo "Error: Failed to copy $SCRIPT_NAME to $TARGET_USER_DIR."
     exit 1
 fi
 
-# Make the script executable
-chmod +x "$TARGET_DIR/$TARGET_SCRIPT"
+# Make the script executable in the user's directory
+chmod +x "$TARGET_USER_DIR/$TARGET_SCRIPT"
 
 # Check if the permission change was successful
 if [ $? -ne 0 ]; then
-    echo "Error: Failed to make $TARGET_SCRIPT executable."
+    echo "Error: Failed to make $TARGET_SCRIPT executable in $TARGET_USER_DIR."
+    exit 1
+fi
+
+# Copy the script to the system-wide directory with the desired name
+sudo cp "$SCRIPT_PATH" "$TARGET_SYSTEM_DIR/$TARGET_SCRIPT"
+
+# Check if the system-wide copy was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to copy $SCRIPT_NAME to $TARGET_SYSTEM_DIR. Try running with sudo."
+    exit 1
+fi
+
+# Make the script executable in the system-wide directory
+sudo chmod +x "$TARGET_SYSTEM_DIR/$TARGET_SCRIPT"
+
+# Check if the permission change was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to make $TARGET_SCRIPT executable in $TARGET_SYSTEM_DIR. Try running with sudo."
     exit 1
 fi
 
 # Display a success message
-echo "Auto Betty script has been installed and can be used globally."
+echo "Auto Betty script has been installed and can be used both locally and globally."
